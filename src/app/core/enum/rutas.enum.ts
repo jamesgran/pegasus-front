50b6dd13fa0d6d3da086a485788986aabac5e55b
JameGran <tamef.granados@gmail.com>
2024-03-15 17:30:33 -0500

    añadido archivo enviroment y rutas enum

---
+++ src/app/core/enum/rutas.enum.ts
@@ -0,0 +1,10 @@
+export enum RUTAS{
+    AUTENTICACION = 'auth/login',
+    INICIO = 'home',
+    ACERCA_DE = 'home/acercade',
+    CONTACTO = 'home/contacto',
+    CLIENTES = 'home/clientes',
+    ADD_CLIENTES = 'home/add-cliente',
+    SERVICIOS = 'home/servicio'
+
+}
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+//aqui van las ips o endpoint o url de las apis a consumir
+export const environment = {
+    production: false,
+    base_url: "http://localhost:4200/api/v1",
+
+}
